7329130d07da196b618b8366f5f01bb15a7f8704
nada-yahya <nadamousa969@g.com>
2022-08-16 16:03:36 +0200

    cards

--- src/app/pages/pro-list/pro-list.component.ts
+++ src/app/pages/pro-list/pro-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CardsService } from 'src/app/services/cards.service';
 
 @Component({
   selector: 'app-pro-list',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProListComponent implements OnInit {
 
-  constructor() { }
+  // cardI:any[]=[];
+  // card: any;
 
+  // infoReceived1: string[]=[];
+ 
+
+  // constructor(private cardservice:CardsService, private dservice: CardsService) { }
+
+  // ngOnInit(): void {
+  //   this.getcardI()
+  // }
+
+  // getcardI(){
+  //   this.cardservice.getcard().subscribe(
+  //     data ​=> {
+  //       this.cardI = data;
+  //       console.log('Data: ', this.cardI)
+  //   },
+  //   err => {
+  //       console.log('Error: ', err)
+  //   },
+  //   () => console.log('Complete')
+  //   )
+  // }
+
+  // getInfoFromService1(){
+
+  //   this.infoReceived1 = this.dservice.getInfo1()
+
+  // }
+
+  constructor(){}
   ngOnInit(): void {
+    
   }
-
 }
